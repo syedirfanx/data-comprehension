66c45f2ca873da3fba1ccd6845fc06000178417a
syedirfanx <irfansyed479@gmail.com>
2022-12-10 13:09:07 +0000

    Update data comprehension using R

--- data-comprehension-using-R.r
+++ data-comprehension-using-R.r
@@ -1,4 +1,4 @@
-install.packages('tidyr')
+#install.packages('tidyr')
 library(tidyr)
 head(who)
 
@@ -7,23 +7,25 @@ who1 <- who %>%
   pivot_longer(
     cols = new_sp_m014:newrel_f65, 
     names_to = "key", 
-    values_to = "cases", 
+    values_to = "cases",
     values_drop_na = TRUE
   )
 head(who1)
 
 library(dplyr)
 who2 <- who1 %>% 
-  mutate(key = stringr::str_replace(key, "new_rel", "newrel"))
+  mutate(key = stringr::str_replace(key, "newrel", "new_rel"))
 head(who2)
 
-whο3 <- who2 %>% 
+who3 <- who2 %>% 
   separate(key, c("new", "type", "sexage"), sep = "_")
 head(who3)
 
 who4 <- who3 %>% 
   separate(sexage, c("sex", "age"), sep = 1)
-
+head(who4, 5)
+tail(who4, 5)
+write.csv(who4, "C:/Users/syedi/data-comprehension/data/who4.csv", row.names=FALSE)
 
 #Task 2
 
@@ -57,9 +59,8 @@ hist(Nile,
      col = "darkmagenta")
 
 qqnorm(Nile)
-qqline(Nile, col="red", lwd = 2) #Normally distributed
-qqplot(Nile)
-table(Nile)
+qqline(Nile, col="red", lwd = 2)
+
 
 plot(Nile, xlab="NILE", ylab="FREQUENCY", main="NILE PLOT", type= "b")
 plot(Nile, xlab="NILE", ylab="FREQUENCY", main="NILE PLOT", type= "l")
@@ -67,3 +68,22 @@ plot(Nile, xlab="NILE", ylab="FREQUENCY", main="NILE PLOT", type= "o")
 plot(Nile, xlab="NILE", ylab="FREQUENCY", main="NILE PLOT", type= "h")
 plot(Nile, xlab="NILE", ylab="FREQUENCY", main="NILE PLOT", type= "p")
 plot(Nile, xlab="NILE", ylab="FREQUENCY", main="NILE PLOT", type= "s")
+
+#install.packages('ggplot2')
+
+#Task3
+library(ggplot2)
+print(mpg)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = cty, y = hwy, color=cty)) +
+  facet_wrap(~ manufacturer, nrow = 2)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, color=displ)) +
+  facet_wrap(. ~ class, nrow = 2)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, color=cyl)) +
+  facet_grid(drv ~ cty)
+
